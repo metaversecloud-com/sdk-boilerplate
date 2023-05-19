5931fab3f67c72586c04935c1897af501e088560
Daniel Liebeskind <daniel.liebeskind@gmail.com>
2023-05-18 19:49:37 -0700

    Improves routes

--- server/routes.js
+++ server/routes.js
@@ -14,6 +14,7 @@ import {
   updateUserDataObject,
   // VISITOR CLASS
   getVisitor,
+  moveVisitor,
   updateLastVisited,
   updateVisitorDataObject,
   // WORLD CLASS
@@ -35,15 +36,16 @@ router.get("/", (req, res) => {
 router.get("/dropped-asset", getEmbeddedAssetDetails); // { includeDataObject: boolean }
 // Gets all dropped assets with unique name
 router.get("/dropped-asset/uniqueNameSearch", fetchDroppedAssetsUniqueName); // { isPartial: boolean, uniqueName: string }
-router.post("/dropped-asset", dropAsset); // { id: string, isInteractive: boolean, position: {x: number, y: number }, uniqueName: string }
 router.post("/dropped-asset/webimage", dropWebImageAsset); // { isInteractive: boolean, layers: {bottom: string, top: string}, position: {x: number, y: number }, uniqueName: string }
 router.post("/dropped-asset/removeAllWithUniqueName", removeDroppedAssetsUniqueName); // { isPartial: boolean, uniqueName: string }
+router.post("/dropped-asset", dropAsset); // { id: string, isInteractive: boolean, position: {x: number, y: number }, uniqueName: string }
 
 // Dropped Asset Instance
-router.get("/dropped-asset/:instanceId", getDroppedAssetDetails); // { includeDataObject: boolean }
-router.delete("/dropped-asset/:instanceId", removeDroppedAsset);
+// /get/:instanceId probably isn't the best way to do this.
+router.post("/dropped-asset/get/:instanceId", getDroppedAssetDetails); // { includeDataObject: boolean }
 router.put("/dropped-asset/:instanceId/updateClickType", updateClickType); // See file for inputs
 router.put("/dropped-asset/:instanceId/updatePosition", updatePosition); // { position: {x, y} }
+router.delete("/dropped-asset/:instanceId", removeDroppedAsset);
 
 // User
 router.get("/user/:profileId", getUser);
@@ -53,13 +55,14 @@ router.put("/user/:profileId/data", updateUserDataObject); // { dataObject: obje
 // visitorId comes from interactive nonce
 router.get("/visitor", getVisitor); // { includeDataObject: boolean }
 router.put("/visitor/last-visited", updateLastVisited);
+router.put("/visitor/move", moveVisitor);
 router.put("/visitor/data", updateVisitorDataObject); // { dataObject: object }
 
 // World
 // urlSlug comes from interactive nonce
-router.get("/world", getWorldDetails); // { includeDataObject: boolean }
-router.put("/world", updateWorldDetails); // See file for inputs
 router.get("/world/data-object", getWorldDataObject); // Does not include world details
 router.put("/world/data-object", updateWorldDataObject); // { dataObject: object }
+router.get("/world", getWorldDetails); // { includeDataObject: boolean }
+router.put("/world", updateWorldDetails); // See file for inputs
 
 export default router;
