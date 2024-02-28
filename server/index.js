773bce33d1014a71a9ee54b10f6bf4d75a1b6f44
abel-tefera <abeltefera16@gmail.com>
2024-02-28 11:42:38 +0300

    Client boilerplate reworked

--- server/index.js
+++ server/index.js
@@ -1,12 +1,9 @@
-import path from "path";
-import { fileURLToPath } from "url";
 import express from "express";
-import bodyParser from "body-parser";
+import cors from "cors";
 import dotenv from "dotenv";
 import router from "./routes.js";
-import cors from "cors";
 import { cleanReturnPayload } from "./utils/cleanReturnPayload.js";
-dotenv.config();
+dotenv.config({ path: "../.env" });
 
 function checkEnvVariables() {
   const requiredEnvVariables = ["INTERACTIVE_KEY", "INTERACTIVE_SECRET"];
@@ -20,11 +17,12 @@ function checkEnvVariables() {
 }
 checkEnvVariables();
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 const app = express();
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 if (process.env.NODE_ENV === "development") {
   const corsOptions = {
@@ -50,7 +48,10 @@ app.use(function (req, res, next) {
   res.send = function (data) {
     if (data) {
       try {
-        const cleanData = cleanReturnPayload(typeof data === "string" ? JSON.parse(data) : data, "topia");
+        const cleanData = cleanReturnPayload(
+          typeof data === "string" ? JSON.parse(data) : data,
+          "topia"
+        );
         res.send = ogSend;
         return res.send(cleanData);
       } catch (error) {
@@ -65,5 +66,5 @@ app.use(function (req, res, next) {
 app.use("/api", router);
 
 app.listen(PORT, () => {
-  console.log(`Server listening on ${PORT}`);
+  console.log(`Server is running on port: ${PORT}`);
 });
