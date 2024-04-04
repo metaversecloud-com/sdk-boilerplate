f83e1402f495046c8ec16f9656a8383dbc665bdc
Lina <linabell@mail.com>
2024-04-04 09:27:36 -0700

    clean up creds check

--- server/utils/getCredentials.js
+++ server/utils/getCredentials.js
@@ -1,17 +1,33 @@
-export const getCredentials = (query) => {
-  const requiredFields = ["interactiveNonce", "interactivePublicKey", "urlSlug", "visitorId"];
-  const missingFields = requiredFields.filter((variable) => !query[variable]);
-  if (missingFields.length > 0) {
-    throw new Error(`Missing required parameters: ${missingFields.join(", ")}`);
-  }
+import { errorHandler } from "./errorHandler.js";
 
-  return {
-    assetId: query.assetId,
-    interactiveNonce: query.interactiveNonce,
-    interactivePublicKey: query.interactivePublicKey,
-    profileId: query.profileId,
-    urlSlug: query.urlSlug,
-    username: query.username,
-    visitorId: Number(query.visitorId),
-  };
+export const getCredentials = (query) => {
+  try {
+    const requiredFields = ["interactiveNonce", "interactivePublicKey", "urlSlug", "visitorId"];
+    const missingFields = requiredFields.filter((variable) => !query[variable]);
+    if (missingFields.length > 0) {
+      throw `Missing required body parameters: ${missingFields.join(", ")}`;
+    }
+
+    if (process.env.INTERACTIVE_KEY !== query.interactivePublicKey) {
+      throw "Provided public key does not match";
+    }
+
+    return {
+      assetId: query.assetId,
+      displayName: query.displayName,
+      interactiveNonce: query.interactiveNonce,
+      interactivePublicKey: query.interactivePublicKey,
+      profileId: query.profileId,
+      sceneDropId: query.sceneDropId,
+      urlSlug: query.urlSlug,
+      username: query.username,
+      visitorId: Number(query.visitorId),
+    };
+  } catch (error) {
+    return errorHandler({
+      error,
+      functionName: "getCredentials",
+      message: "Error getting credentials from query.",
+    });
+  }
 };
